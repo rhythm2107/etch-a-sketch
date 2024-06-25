e5b42f8cfc3696a0c2de2a33c98ed9d3b391480c
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-06-25 23:08:35 +0200

    Create a toggle logic function

--- script.js
+++ script.js
@@ -12,10 +12,90 @@ function createGrid(gridSize) {
         item.style.setProperty('--item-height', `${itemHeight}%`)
         container.appendChild(item)
 
-        item.addEventListener('mouseover', () => {
-            item.style.backgroundColor = 'red';
-        })
+       
     }
 }
 
-createGrid(65)
+function clearGrid() {
+    const container = document.querySelector('.grid-container')
+    const slider = document.getElementById('myRange')
+    
+    createGrid(slider.value)
+}
+
+const clearGridButton = document.getElementById('clear-grid')
+clearGridButton.addEventListener('click', function() {
+    clearGrid()
+})
+
+createGrid(25)
+
+const slider = document.getElementById('myRange')
+const gridSizeElements = document.querySelectorAll('.grid-size')
+
+slider.addEventListener('input', function() {
+    gridSizeElements.forEach(function(element) {
+        element.textContent = slider.value
+        createGrid(slider.value)
+    })
+})
+
+const gridContainer = document.querySelector('.grid-container');
+let mouseIsDown = false;
+
+gridContainer.addEventListener('mousedown', function(event) {
+    event.preventDefault()
+    mouseIsDown = true;
+    if (event.target.classList.contains('grid-item')) {
+        event.target.style.backgroundColor = selectedColor;
+    }
+});
+
+gridContainer.addEventListener('mouseup', function() {
+    mouseIsDown = false;
+});
+
+gridContainer.addEventListener('mouseover', function(event) {
+    if (mouseIsDown && event.target.classList.contains('grid-item')) {
+        event.target.style.backgroundColor = selectedColor;
+    }
+});
+
+
+const colorPicker = document.getElementById('colorPicker')
+let selectedColor = '#ff0000'
+
+colorPicker.addEventListener('change', function() {
+    selectedColor = this.value;
+    console.log("Selected color:", selectedColor)
+})
+
+function rgbToHex(r, g, b) {
+    return "#" + ((r << 16) + (g << 8) + b).toString(16).padStart(6, '0');
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const rgbColor = rgbToHex(r, g, b)
+    console.log(rgbColor)
+    return rgbColor
+}
+
+let rainbowMode = 'Off'
+rainbow = document.getElementById('rainbow')
+
+
+rainbow.addEventListener('click', function() {
+    if (rainbowMode === 'Off') {
+        rainbowMode = 'On';
+        this.classList.remove('button-off');
+        this.classList.add('button-on');
+    } else {
+        rainbowMode = 'Off';
+        this.classList.remove('button-on');
+        this.classList.add('button-off');
+    }
+});
